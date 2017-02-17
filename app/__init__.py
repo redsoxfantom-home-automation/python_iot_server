cba6b3f5996fc686cf8412a644db8bf8aa0c9850
Tom English <redsoxfantom@gmail.com>
2017-02-17 18:27:02 -0500

    Added logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from python_utils.zookeeper import ServiceAccessor
 from app.api_1_0 import api_1_0
+import logging
 
 dns = ServiceAccessor()
 dns.connect()
@@ -8,3 +9,5 @@ dns.register_service("1.0","lights","5000")
 
 app = Flask(__name__)
 app.register_blueprint(api_1_0.bp, url_prefix='/1.0')
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
