5a2485f4e32ef922b9414fead8b81c0c2114644d
Tom English <redsoxfantom@gmail.com>
2017-02-17 19:18:40 -0500

    Improved logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from python_utils.zookeeper import ServiceAccessor
 from app.api_1_0 import api_1_0
 import logging
 import os
+import sys
 
 # load configuration to tell us what port to use
 root = os.path.realpath(os.path.dirname(__file__))
@@ -15,3 +16,9 @@ dns.register_service("1.0","lights",str(config_data["port"]))
 
 app = Flask(__name__)
 app.register_blueprint(api_1_0.bp, url_prefix='/1.0')
+formatter = logging.Formatter("%(levelname)s : [%(module)s] : %(message)s")
+stdOutHandler = logging.StreamHandler(sys.stdout)
+stdOutHandler.setFormatter(formatter)
+app.logger.addHandler(stdOutHandler)
+app.logger.setLevel(logging.DEBUG)
+
