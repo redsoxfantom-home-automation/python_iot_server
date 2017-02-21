9a8a69554ae881a331af9da4174b01f9fd44afeb
Tom English <redsoxfantom@gmail.com>
2017-02-20 21:16:55 -0500

    Added error handling to light retrieval

--- app/api_1_0/lifx_helper.py
+++ app/api_1_0/lifx_helper.py
@@ -1,81 +1,85 @@
 import lifx
+import logging
 from lifx.color import HSBK
 
 client = lifx.Client()
 
 known_bulbs = [
-	{
-		'vid':1,
-		'name':'LIFX',
-		'products':[
-			{
-				'pid':1,
-				'name':'Original 1000',
-				'features': {
-					'color':True
-				}
-			}
-		]
-	}
+   {
+      'vid':1,
+      'name':'LIFX',
+      'products':[
+         {
+            'pid':1,
+            'name':'Original 1000',
+            'features': {
+               'color':True
+            }
+         }
+      ]
+   }
 ]
 
 class lightJsonClass(object):
-	def __init__(self,light):
-		self.label = light.label
-		self.id = light.id
-		self.power = light.power
-		self.hue = light.hue
-		self.saturation = light.saturation
-		self.brightness = light.brightness
-		self.kelvin = light.kelvin
-		self.name = "UNKNOWN"
-		self.vendor = "UNKNOWN"
-		self.features = {}
+   def __init__(self,light):
+      self.label = light.label
+      self.id = light.id
+      self.power = light.power
+      self.hue = light.hue
+      self.saturation = light.saturation
+      self.brightness = light.brightness
+      self.kelvin = light.kelvin
+      self.name = "UNKNOWN"
+      self.vendor = "UNKNOWN"
+      self.features = {}
 
-		properties = light.properties
-		bulb_data = [o for o in known_bulbs if o['vid'] == properties.vendor]
-		if bulb_data:
-			bulb_data = bulb_data[0]
-			self.vendor = bulb_data['name']
-			bulb_data = [o for o in bulb_data['products'] if o['pid'] == properties.product]
-			if bulb_data:
-				bulb_data = bulb_data[0]
-				self.name = bulb_data['name']
-				self.features = bulb_data['features']
+      properties = light.properties
+      bulb_data = [o for o in known_bulbs if o['vid'] == properties.vendor]
+      if bulb_data:
+         bulb_data = bulb_data[0]
+         self.vendor = bulb_data['name']
+         bulb_data = [o for o in bulb_data['products'] if o['pid'] == properties.product]
+         if bulb_data:
+            bulb_data = bulb_data[0]
+            self.name = bulb_data['name']
+            self.features = bulb_data['features']
 
 def get_all_lights():
-	retList = []
-	for l in client.get_devices():
-		new_l = lightJsonClass(l)
-		retList.append(new_l)	
-	return retList
+   retList = []
+   for l in client.get_devices():
+      try:
+         new_l = lightJsonClass(l)
+         retList.append(new_l)
+      except Exception as err:
+         logging.error("Error occured while getting light: {0}".format(err))
+   return retList
 
-def get_light(light_id):	
-	l = client.by_id(light_id)
-	new_l = lightJsonClass(l)
-	return new_l
+def get_light(light_id):   
+   l = client.by_id(light_id)
+   new_l = lightJsonClass(l)
+   return new_l
 
 def update_light(light_id,attributes):
-	l = client.by_id(light_id)
-	
-	hue = l.color.hue
-	brightness = l.color.brightness
-	saturation = l.color.saturation
-	kelvin = l.color.kelvin
+   l = client.by_id(light_id)
+   
+   hue = l.color.hue
+   brightness = l.color.brightness
+   saturation = l.color.saturation
+   kelvin = l.color.kelvin
 
-	for key in attributes:
-		if(key == 'label'):
-			l.label = attributes[key]
-		if(key == 'power'):
-			l.power = (attributes[key] == 'True')
-		if(key == 'brightness'):
-			brightness = float(attributes[key])
-		if(key == 'hue'):
-			hue = float(attributes[key])
-		if(key == 'kelvin'):
-			kelvin = int(attributes[key])
-		if(key == 'saturation'):
-			saturation = float(attributes[key])
+   for key in attributes:
+      if(key == 'label'):
+         l.label = attributes[key]
+      if(key == 'power'):
+         l.power = (attributes[key] == 'True')
+      if(key == 'brightness'):
+         brightness = float(attributes[key])
+      if(key == 'hue'):
+         hue = float(attributes[key])
+      if(key == 'kelvin'):
+         kelvin = int(attributes[key])
+      if(key == 'saturation'):
+         saturation = float(attributes[key])
 
-	new_color = HSBK(hue,saturation,brightness,kelvin)
-	l.color = new_color
+   new_color = HSBK(hue,saturation,brightness,kelvin)
+   l.color = new_color
